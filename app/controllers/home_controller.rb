52757bce110348fbda16b0458071ce41e4b317ce
grantbradshaw <grant.bradshaw@outlook.com>
2018-10-04 12:18:14 -0400

    WIP scraping of hacker news API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,31 @@
 class HomeController < ApplicationController
+  require 'net/http'
+  require 'json'
+
   def index
+    @articles = get_articles
   end
-end
 
+  def get_articles
+    begin
+      uri = URI('https://hacker-news.firebaseio.com/v0/topstories.json')
+      res = Net::HTTP.get(uri)
+      top_articles = JSON.parse(res)
+      top_30_articles = top_articles[0,30].map {|a| get_article(a)}
+      return top_30_articles
+    rescue => e
+      puts "failed #{e}"
+    end
+  end
 
-class HackerNews
+  def get_article(id)
+    uri = URI("https://hacker-news.firebaseio.com/v0/item/#{id}.json")
+    res = Net::HTTP.get(uri)
+    article = JSON.parse(res)
+    return {
+      :title => article["title"],
+      :url => article["url"]
+    }
+  end
+
 end
