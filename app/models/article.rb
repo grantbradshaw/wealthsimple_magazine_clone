76aa18d60950884d0e7ad11bf1a364ccd8250754
grantbradshaw <grant.bradshaw@outlook.com>
2018-10-08 09:54:31 -0400

    Writing model validations and related tests.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,62 +1,72 @@
 class Article < ApplicationRecord
-  require 'net/http'
-  require 'json'
-  require 'nokogiri'
+  validates :title, presence: true
+  validates :url, presence: true, format: {
+    with: /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/,
+    message: 'must be a valid url'
+  }
+  validates :image_link, :allow_blank => true, format: {
+    with: /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/,
+    message: 'must be a valid url'
+  }
 
-  def index
-    @articles = []
-  end
+  # require 'net/http'
+  # require 'json'
+  # require 'nokogiri'
 
-  def get_articles
-    begin
-      uri = URI('https://hacker-news.firebaseio.com/v0/topstories.json')
-      res = Net::HTTP.get(uri)
-      top_articles = JSON.parse(res)
-      top_30_articles = top_articles[0,30].map {|a| get_article(a)}
-      return top_30_articles
-    rescue => e
-      puts "failed #{e}"
-    end
-  end
+  # def index
+  #   @articles = []
+  # end
 
-  def get_article(id)
-    begin
-      uri = URI("https://hacker-news.firebaseio.com/v0/item/#{id}.json")
-      res = Net::HTTP.get(uri)
-      article = JSON.parse(res)
+  # def get_articles
+  #   begin
+  #     uri = URI('https://hacker-news.firebaseio.com/v0/topstories.json')
+  #     res = Net::HTTP.get(uri)
+  #     top_articles = JSON.parse(res)
+  #     top_30_articles = top_articles[0,30].map {|a| get_article(a)}
+  #     return top_30_articles
+  #   rescue => e
+  #     puts "failed #{e}"
+  #   end
+  # end
 
-      article_url = article["url"]
-      article_title = article["title"]
+  # def get_article(id)
+  #   begin
+  #     uri = URI("https://hacker-news.firebaseio.com/v0/item/#{id}.json")
+  #     res = Net::HTTP.get(uri)
+  #     article = JSON.parse(res)
 
-      return {
-        :title => article_title,
-        :url => article_url,
-        :image => get_article_image(article_url)
-        # :image => true
-      }
-    rescue => e
-      puts "failed #{e}"
-    end
-  end
+  #     article_url = article["url"]
+  #     article_title = article["title"]
 
-  def get_article_image(url)
-    begin
-      uri = URI(url)
-      res = Net::HTTP.get(uri)
-      doc = Nokogiri::HTML(res)
+  #     return {
+  #       :title => article_title,
+  #       :url => article_url,
+  #       :image => get_article_image(article_url)
+  #       # :image => true
+  #     }
+  #   rescue => e
+  #     puts "failed #{e}"
+  #   end
+  # end
 
-      doc.search('head meta').each do |e|
-        puts e
-        if e.attribute('property') == 'og:image'
-          puts e.attribute('content')
-          return e.attribute('content')
-        else
-          return nil
-        end
-      end
-    rescue => e
-      puts "failed #{e}"
-      return nil
-    end
-  end
+  # def get_article_image(url)
+  #   begin
+  #     uri = URI(url)
+  #     res = Net::HTTP.get(uri)
+  #     doc = Nokogiri::HTML(res)
+
+  #     doc.search('head meta').each do |e|
+  #       puts e
+  #       if e.attribute('property') == 'og:image'
+  #         puts e.attribute('content')
+  #         return e.attribute('content')
+  #       else
+  #         return nil
+  #       end
+  #     end
+  #   rescue => e
+  #     puts "failed #{e}"
+  #     return nil
+  #   end
+  # end
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @article = Article.new(:title => 'Why Do Computers Use So Much Energy?',
+                          :url => 'https://blogs.scientificamerican.com/observations/why-do-computers-use-so-much-energy/',
+                          :image_link => 'https://static.scientificamerican.com/blogs/cache/file/36E8122B-F737-477F-9F3BE740816FAF6F.jpg')
+  end
+
+  test 'valid article with image_link' do
+    assert @article.valid?
+  end
+
+  test 'valid article without image_link' do
+    @article.image_link = nil
+    assert @article.valid?
+  end
+
+  test 'invalid without title' do
+    @article.title = nil
+    refute @article.valid?, 'article is valid without a title'
+    assert_not_nil @article.errors[:title], 'no validation error for title present'
+  end
+
+  test 'invalid without url' do
+    @article.url = nil
+    refute @article.valid?, 'article is valid without a url'
+    assert_not_nil @article.errors[:title], 'no validation error for url present'
+  end
+
+  test 'invalid article url' do
+    @article.url = 'The quick brown fox'
+    refute @article.valid?, 'article is valid with a url that doesn\'t match a url regex'
+    assert_not_nil @article.errors[:url], 'no validation error for url present'
+  end
+
+  test 'invalid article image link' do
+    @article.image_link = 'The slow green pig'
+    refute @article.valid?, 'article is valid with an image_link that doesn\'t match a url regex'
+    assert_not_nil @article.errors[:image_link], 'no validation error for image_link present'
+  end
 end
